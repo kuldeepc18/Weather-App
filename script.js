cd30b91ded19f0ccdfe648bee4b1ab8370d81f0f
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-08-15 01:24:45 +0530

    Update Script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cityInput = document.getElementById('city-input'),
     searchBtn = document.getElementById('searchBtn'),
     locationBtn = document.getElementById('locationBtn'),
-    api_key = process.env.API_KEY,
+    api_key = '<Your-API-Key>',
     currentWeatherCard = document.querySelectorAll('.weather-left .card')[0],
     fiveDaysForecastCard = document.querySelector('.day-forecast'),
     aqiCard = document.querySelectorAll('.highlights .card')[0],
